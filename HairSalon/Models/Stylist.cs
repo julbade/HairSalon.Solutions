3fc0f2513745cbd11d27411635f8e5c97d5510d0
julius bade <julbade21@gmail.com>
2018-09-21 09:23:04 -0700

    first commit

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using ToDoList;
+using MySql.Data.MySqlClient;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    private int _id;
+    private string _description;
+
+    public Item(string Description, int Id = 0)
+    {
+      _id = Id;
+      _description = Description;
+    }
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item) otherItem;
+        bool idEquality = (this.GetId() == newItem.GetId());
+        bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
+        return (idEquality && descriptionEquality);
+      }
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+
+        conn.Close();
+        if (conn != null)
+        {
+        conn.Dispose();
+        }
+      return allItems;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = this._description;
+      cmd.Parameters.Add(description);
+      // // more logic will go here
+      //
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      //
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"TRUNCATE TABLE items;";
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+    public static Item Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemDescription = "";
+
+      while (rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemDescription = rdr.GetString(1);
+      }
+      Item foundItem = new Item(itemDescription, itemId);
+
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundItem;
+    }
+    public void Edit(string newDescription)
+    {
+      MySqlConnection conn = DB.Connection();
+           conn.Open();
+           var cmd = conn.CreateCommand() as MySqlCommand;
+           cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+
+           MySqlParameter searchId = new MySqlParameter();
+           searchId.ParameterName = "@searchId";
+           searchId.Value = _id;
+           cmd.Parameters.Add(searchId);
+
+           MySqlParameter description = new MySqlParameter();
+           description.ParameterName = "@newDescription";
+           description.Value = newDescription;
+           cmd.Parameters.Add(description);
+
+           cmd.ExecuteNonQuery();
+           _description = newDescription;
+
+           conn.Close();
+           if (conn != null)
+           {
+               conn.Dispose();
+           }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+# Hair Salon
+
+#### A Hair Salon Program.
+
+#### By **Julius Bade**
+
+## Description
+  An app for a hair salon. The owner should be able to add a list of the stylists, and for each stylist, add clients who see that stylist. The stylists work independently, so each client only belongs to a single stylist.
+
+
+### Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **Program Gathers Name of Stylists** | Stylist 1: "Julius" | Stylist 2: "Mark" |
+| **Program Gathers Name of Clients for each Stylists** | Client 1: "Jake" | Client 2 : "Jen"|
+| **Program Adds Client for each Specific Sylist** | Client 2: "John" | Client 2 : "Alice"|
+| **Program Adds Client for each Specific Sylist** | Client 3: "Sam" | Client 2 : "N/A"|
+
+
+## Setup/Installation Requirements
+
+1. Clone this repository at
+2. Go to folder directory HairSalon.Solution/HairSalon
+3. Open MAMP and start servers
+4. Go to MySQL from terminal
+5. > CREATE DATABASE hair_salon;
+6. > USE hair_salon;
+7. > CREATE TABLE stylists (id serial PRIMARY KEY, name VARCHAR(255));
+8. > CREATE TABLE clients (id serial PRIMARY KEY, name VARCHAR(255));
+9. Run through local host by typing "dotnet run"
+10. Open Web Browser (Chrome, Mozilla, Safari, etc.)
+11. Open http://localhost:5000 in web browser.
+
+## Known Bugs
+* No known bugs at this time.
+
+## Technologies Used
+* C-Sharp
+* MS Test
+* Netcoreapp 1.1
+* Atom
+* GitHub
+* MS MVC
+* MySQL
+* myPhpAdmin
+
+
+## Support and contact details
+
+
+_Julius Bade julbade21@gmail.com_
+
+### License
+
+*{This software is licensed under the MIT license}*
+
+Copyright (c) 2018 **_{Julius Bade}_**
